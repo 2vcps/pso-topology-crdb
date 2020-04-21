af7f7c12bea59d1d1cda69fc0aa38f5cca493ce9
Disk Thanos <jon.owings@gmail.com>
2020-04-21 15:16:52 -0700

    test

--- crdb-init.sh
+++ crdb-init.sh
@@ -1,1 +1,3 @@
 kubectl create -f https://raw.githubusercontent.com/cockroachdb/cockroach/master/cloud/kubernetes/cluster-init.yaml
+
+# kubectl delete -f https://raw.githubusercontent.com/cockroachdb/cockroach/master/cloud/kubernetes/cluster-init.yaml
