e00500d653cd32525faa730c0558033817316acf
Jon Owings <owings@purestorage.com>
2020-06-17 09:58:36 -0400

    updates-local

--- meetup/resize.md
+++
@@ -1,3 +0,0 @@
-kubectl get pvc -l app=cockroachdb -o name | xargs kubectl patch -p='{"spec": {"resources": {"requests": {"storage": "2000Gi"}}}}'
-
-kubectl rollout restart statefulset cockroachdb
--- meetup/tag-servers.sh
+++
@@ -1,6 +0,0 @@
-kubectl label node dev-0 topology.purestorage.com/dinner=tacos
-kubectl label node dev-1 topology.purestorage.com/dinner=burgers
-kubectl label node dev-2 topology.purestorage.com/dinner=tacos
-kubectl label node dev-3 topology.purestorage.com/dinner=tacos
-kubectl label node dev-4 topology.purestorage.com/dinner=burgers
-kubectl label node dev-5 topology.purestorage.com/dinner=burgers
